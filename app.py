41e8ea979481212780811aa70eac2f5b03a4f2a6
Mélanie Delaunay <melaniedelaunay816@gmail.com>
2023-06-10 20:31:26 +0200

    modified:   app.py 	modified:   templates/dashboard/a_propos.html.j2 	modified:   templates/dashboard/index.html.j2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 db = SQLAlchemy(app)
 
 #Création de models
-class todo(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean, default=False)
@@ -16,19 +16,32 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    todo_list = todo.query.all()
-    return render_template('dashboard/index.html.j2', todo_list=todo_list)
+    todo_list = Todo.query.all()
+    total_todo = Todo.query.count()
+    fait_todo = Todo.query.filter_by(complete=True).count()
+    pasfait_todo = Todo.query.filter_by(complete=False).count()
+    return render_template('dashboard/index.html.j2', **locals())
 
 @app.route('/ajouter', methods=['POST'])
 def ajouter():
     title = request.form.get('title')
-    new_todo = todo(title=title, complete=False)
+    new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit() 
     return redirect(url_for('index'))
 
-@app.route('/supprimer')
-def supprimer():
+@app.route('/supprimer/<int:id>')
+def supprimer(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/modifier/<int:id>')
+def modifier(id):
+    todo = Todo.query.filter_by(id=id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/a_propos')
